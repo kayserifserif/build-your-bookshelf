227957156339cddef9c0a26b3bedf874431d41a7
whykatherine <whykatherine@gmail.com>
2021-01-09 00:38:39 +0800

    feat: text based on book data

--- src/Canvas.js
+++ src/Canvas.js
@@ -4,73 +4,95 @@ function Canvas(props) {
 
   const canvasRef = useRef(null);
 
-  const draw = (ctx, frameCount, mode, colors) => {
-
-    const title = "Things Fall Apart";
-    const author = "Chinua Achebe";
-    const publisher = "aws";
-
-    switch (mode) {
-
-      case "colourBlock":
-        if (colors && colors.hasOwnProperty("vibrant")) {
-          ctx.fillStyle = colors.darkVibrant;
-        }
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-        ctx.fillStyle = "#ffffff";
-
-        ctx.save();
-
-        ctx.translate(ctx.canvas.width * 0.5, ctx.canvas.height * 0.5);
-        ctx.rotate((Math.PI / 180) * 90);
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "left";
-        ctx.font = "16px serif";
-        ctx.fillText(title, -ctx.canvas.height * 0.5 + 20, 0);
-        ctx.textAlign = "right";
-        ctx.font = "12px serif";
-        ctx.fillText(author, ctx.canvas.height * 0.5 - 40, 0);
-        ctx.restore();
-
-        ctx.textAlign = "center";
-        ctx.font = "11px serif";
-        ctx.fillText(publisher, ctx.canvas.width * 0.5, ctx.canvas.height - 20);
-        break;
-
-      case "colourGradient":
-        let grad = ctx.createLinearGradient(0, 0, ctx.canvas.width, ctx.canvas.height);
-        if (colors && colors.hasOwnProperty("darkVibrant")) {
-          grad.addColorStop(0, colors.darkVibrant);
-          grad.addColorStop(1, colors.lightVibrant);
-        }
-        ctx.fillStyle = grad;
-        ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-        ctx.fillStyle = "#ffffff";
-
-        ctx.save();
-
-        ctx.translate(ctx.canvas.width * 0.5, ctx.canvas.height * 0.5);
-        ctx.rotate((Math.PI / 180) * 90);
-        ctx.textBaseline = "middle";
-        ctx.textAlign = "left";
-        ctx.font = "16px serif";
-        ctx.fillText(title, -ctx.canvas.height * 0.5 + 20, 0);
-        ctx.textAlign = "right";
-        ctx.font = "12px serif";
-        ctx.fillText(author, ctx.canvas.height * 0.5 - 40, 0);
-        ctx.restore();
-
-        ctx.textAlign = "center";
-        ctx.font = "11px serif";
-        ctx.fillText(publisher, ctx.canvas.width * 0.5, ctx.canvas.height - 20);    
-        break;
-
-      default:
-        break;
+  const draw = (ctx, frameCount, mode, data, colors, image) => {
+    let maxWidth = 0;
+    ctx.font = "16px serif";
+    for (let word of data.title.split(" ")) {
+      maxWidth = Math.max(maxWidth, ctx.measureText(word).width);
     }
-  };
+    ctx.font = "12px serif";
+    for (let word of data.author.split(" ")) {
+      maxWidth = Math.max(maxWidth, ctx.measureText(word).width);
+    }
+    ctx.font = "11px serif";
+    for (let word of data.publisher.split(" ")) {
+      maxWidth = Math.max(maxWidth, ctx.measureText(word).width);
+    }
+
+    if (mode === "colourBlock") colourBlock(ctx, colors);
+    else if (mode === "colourGradient") colourGradient(ctx, colors);
+    else if (mode === "coverCrop") coverCrop(ctx, image);
+    else if (mode === "coverBlur") coverBlur(ctx, image);
+
+    text(ctx, data, maxWidth);
+  }
+
+  const colourBlock = (ctx, colors) => {
+    if (colors && colors.hasOwnProperty("vibrant")) {
+      ctx.fillStyle = colors.darkVibrant;
+    }
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  const colourGradient = (ctx, colors) => {
+    let grad = ctx.createLinearGradient(0, 0, ctx.canvas.width, ctx.canvas.height);
+    if (colors && colors.hasOwnProperty("darkVibrant")) {
+      grad.addColorStop(0, colors.darkVibrant);
+      grad.addColorStop(1, colors.lightVibrant);
+    }
+    ctx.fillStyle = grad;
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  }
+
+  const coverCrop = (ctx, image) => {
+
+  }
+
+  const coverBlur = (ctx, image) => {
+
+  }
+
+  const text = (ctx, data, maxWidth) => {
+    const vertical = (maxWidth > data.pages * 0.3);
+
+    ctx.fillStyle = "#ffffff";
+
+    if (vertical) {
+      ctx.textBaseline = "middle";
+
+      ctx.save();
+      ctx.font = "16px serif";
+      ctx.translate(ctx.canvas.width * 0.5, ctx.canvas.height * 0.5);
+      ctx.rotate((Math.PI / 180) * 90);
+      ctx.textAlign = "left";
+      ctx.fillText(data.title, -ctx.canvas.height * 0.5 + 20, 0);
+      ctx.font = "12px serif";
+      ctx.textAlign = "right";
+      ctx.fillText(data.author, ctx.canvas.height * 0.5 - 50, 0);
+      ctx.restore();
+    } else {
+      ctx.textAlign = "center";
+
+      ctx.font = "16px serif";
+      ctx.textBaseline = "top";
+      let _title = data.title.split(" ");
+      for (let i = 0; i < _title.length; i++) {
+        ctx.fillText(_title[i], ctx.canvas.width * 0.5, 40 + 16 * i);
+      }
+      ctx.font = "12px serif";
+      ctx.textBaseline = "bottom";
+      let _author = data.author.split(" ");
+      for (let i = 0; i < _author.length; i++) {
+        ctx.fillText(_author[_author.length - 1 - i],
+          ctx.canvas.width * 0.5, ctx.canvas.height - 60 - 12 * i);
+      }
+    }
+
+    ctx.font = "11px serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+    ctx.fillText(data.publisher, ctx.canvas.width * 0.5, ctx.canvas.height - 20);
+  }
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -80,7 +102,7 @@ function Canvas(props) {
 
     const render = () => {
       frameCount++;
-      draw(context, frameCount, props.mode, props.colors);
+      draw(context, frameCount, props.mode, props.data, props.colors, props.image);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
--- src/Covers.js
+++ src/Covers.js
@@ -2,45 +2,46 @@ import React from 'react';
 import { usePalette } from 'react-palette';
 
 import Canvas from './Canvas';
+import coverData from './covers/covers.json'
+
+console.log(coverData);
 
 // https://stackoverflow.com/questions/53762640/how-to-import-all-images-from-a-folder-in-reactjs
-function importAll(r) {
-  return r.keys().map(r);
-}
+// function importAll(r) {
+//   return r.keys().map(r);
+// }
+// const images = importAll(require.context('./covers', false, /\.jpg$/));
+// const i = Math.floor(Math.random() * images.length);
 
-const images = importAll(require.context('./covers', false, /\.jpg$/));
-const i = Math.floor(Math.random() * images.length);
+const id = Object.keys(coverData)[Math.floor(Math.random() * Object.keys(coverData).length)];
+const data = coverData[id];
+const path = data.path;
 
 function Covers() {
 
-  // const width = 180;
-  const number_of_pages = 216;
-  const width = number_of_pages * 0.3;
-  const height = 273;
-
-  const { data, loading, error } = usePalette(images[i].default);
+  const palette = usePalette(path);
 
   return (
     <div className="covers">
       <div>
         <p>Original cover</p>
-        <img src={images[i].default} alt="cover" />
+        <img src={path} alt="cover" />
       </div>
       <div>
         <p>Colour block</p>
-        <Canvas width={width} height={height} mode="colourBlock" colors={data} />
+        <Canvas width={data.pages * 0.3} height={data.height} mode="colourBlock" data={data} colors={palette.data} />
       </div>
       <div>
         <p>Colour gradient</p>
-        <Canvas width={width} height={height} mode="colourGradient" colors={data} />
+        <Canvas width={data.pages * 0.3} height={data.height} mode="colourGradient" data={data} colors={palette.data} />
       </div>
       <div>
         <p>Cover crop</p>
-        <Canvas width={width} height={height} mode="coverCrop" />
+        <Canvas width={data.pages * 0.3} height={data.height} mode="coverCrop" data={data} image={path} />
       </div>
       <div>
         <p>Cover blur</p>
-        <Canvas width={width} height={height} mode="coverBlur" />
+        <Canvas width={data.pages * 0.3} height={data.height} mode="coverBlur" data={data} image={path} />
       </div>
     </div>
   );
